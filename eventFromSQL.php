13307e92e530a75af865d3bedf74ca1b997a7ca1
Kevin Jia <jia.kevin42@gmail.com>
2016-10-04 21:13:35 -0400

    changing database config on php file

--- eventFromSQL.php
+++ eventFromSQL.php
@@ -1,17 +1,19 @@
+//each event will have a start time, end time, and title
 <?php
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 
-$conn = new mysqli("myServer", "myUser", "myPassword", "Northwind");
+$conn = new mysqli("myServer", "kevinarlen ", "studentcalendar123", "Northwind");
 
-$result = $conn->query("SELECT CompanyName, City, Country FROM Customers");
+$result = $conn->query("SELECT ID, Title, Start, End FROM Customers");
 
 $outp = "[";
 while($rs = $result->fetch_array(MYSQLI_ASSOC)) {
     if ($outp != "[") {$outp .= ",";}
-    $outp .= '{"Name":"'  . $rs["CompanyName"] . '",';
-    $outp .= '"City":"'   . $rs["City"]        . '",';
-    $outp .= '"Country":"'. $rs["Country"]     . '"}'; 
+    $outp .= '{"ID":"'  . $rs["ID"] . '",';
+    $outp .= '"Title":"'   . $rs["Title"]        . '",';
+    $outp .= '"Start":"'. $rs["Start"]     . '"}'; 
+    $outp .= '"End":"'. $rs["End"]     . '"}'; 
 }
 $outp .="]";
 
