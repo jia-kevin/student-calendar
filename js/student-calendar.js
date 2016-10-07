6782eac6a0fca196bc82c9f5e50e46d84b8fed45
Arlen Yu <arlen.yu6@gmail.com>
2016-10-07 11:12:46 -0400

    added date and time support on onclick events

--- js/student-calendar.js
+++ js/student-calendar.js
@@ -1,20 +1,20 @@
 
 
 $(document).ready(function() {
-        var date = new Date(2016, 10, 10);
-        var d = date.getDate();
-        var m = date.getMonth();
-        var y = date.getFullYear();
+        // var date = new Date(2016, 10, 10);
+        // var d = date.getDate();
+        // var m = date.getMonth();
+        // var y = date.getFullYear();
 
-        $('#button_create').click(function() { //creates new event on button click
-            var newEvent = {
-                title: 'NEW EVENT',
-                start: new Date(y, m, d)
-            };
-            $('#calendar').fullCalendar( 'renderEvent', newEvent , 'stick');
-        });
+        // // $('#button_create').click(function() { //creates new event on button click
+        // //     var newEvent = {
+        // //         title: 'NEW EVENT',
+        // //         start: new Date(y, m, d)
+        // //     };
+        // //     $('#calendar').fullCalendar( 'renderEvent', newEvent , 'stick');
+        // // });
 
-        $('#button_view').click(function() { //changes viewType
+        $('#button_view').click(function() { //changes viewType 
             var view = $('#calendar').fullCalendar('getView');
             if (view.name == 'month') {
                 $('#calendar').fullCalendar( 'changeView','agendaWeek');
@@ -23,12 +23,6 @@ $(document).ready(function() {
             }
         });
 
-        var newEvent = {
-            title: 'MATH135 Midterm',
-            start: new Date(2016, 09, 03),
-    		color: 'yellow',   // an option!
-    		textColor: 'black' // an option!
-        };
 
         function checkTime(time){ //checks whether given time is in valid format
             re = /^\d{1,2}:\d{2}([ap]m)?$/; //regular expression to match required format
@@ -49,19 +43,39 @@ $(document).ready(function() {
 	        events: "application/jsonMySQL.php",
 
             dayClick: function(date, allDay, jsEvent, view) { //onclick event creation
-                vex.dialog.prompt({
-                    message: 'Event Title:',
-                    placeholder: 'Event Title',
-                    callback: function (value) {
-                        //console.log(value)
-                        var title = value;
-                        var newEvent = {
-                            title: title,
-                            start: date
-                        };
-                        $('#calendar').fullCalendar( 'renderEvent', newEvent , 'stick');
+               //  vex.dialog.prompt({
+               //      message: 'Event Title:',
+               //      placeholder: 'Event Title',
+               //      callback: function (value) {
+               //          var title = value;
+               //          var newEvent = {
+               //              title: title,
+               //              start: date
+               //          };
+               //          $('#calendar').fullCalendar( 'renderEvent', newEvent , 'stick');
+               //      }
+               // })
+               vex.dialog.open({
+                input: [
+                    '<label for="text">Event Name:</label>',
+                    '<input name="name" type="text" value="" />',
+                    '<label for="date">Time</label>',
+                        '<input name="time" type="time" value="" />',
+                ].join(''),
+                callback: function (data) {
+                    if (!data) {
+                        return console.log('Cancelled')
                     }
-               })
+                    var title = data.name;
+                    var datetime = date;
+                    var newEvent = {
+                        title:title,
+                        start: datetime //need to manipulate datetime to implement inputted time
+                    }
+                    $('#calendar').fullCalendar( 'renderEvent', newEvent , 'stick');
+
+                }   
+                })
                 
             }
     	})
