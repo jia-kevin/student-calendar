b88834d88065375ce5cf9625ed4d6d7702235364
Arlen Yu <arlen.yu6@gmail.com>
2016-11-15 21:04:18 -0500

    Update eventToSQL.php

--- js/eventToSQL.php
+++ js/eventToSQL.php
@@ -1,19 +1,19 @@
 <?php
 //https://www.jqueryajaxphp.com/fullcalendar-crud-with-jquery-and-php/
-$servername = "student-calendar0.c6nyhmv3ij8y.us-west-2.rds.amazonaws.com"
+$servername = "student-calendar0.c6nyhmv3ij8y.us-west-2.rds.amazonaws.com";
 $username = "kevinarlen";
-$password = "studentcalendar123"
-$dbname = "Student-Calendar0"
+$password = "studentcalendar123";
+$dbname = "Student_Calendar0";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-$type = $_POST['type']
+$type = $_POST['type'];
 
 if($type == 'new') {
 
   	$startdate = $_POST['startdate'].'+'.$_POST['zone'];
   	$title = $_POST['title'];
-  	$insert = mysqli_query($conn,"INSERT INTO calendar(`title`, `startdate`, `enddate`, `allDay`) VALUES('$title','$startdate','$startdate','false')");
+  	$insert = mysqli_query($conn,"INSERT INTO calendar('title', 'startdate', 'enddate', 'allDay') VALUES('$title','$startdate','$startdate','false')");
   	$lastid = mysqli_insert_id($conn);
   	echo json_encode(array('status'=>'success','eventid'=>$lastid));
 }
