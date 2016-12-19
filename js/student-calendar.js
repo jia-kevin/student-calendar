95d0edfab2a36f3f2911a2d152bee02b02de2d93
jia-kevin <jia.kevin42@gmail.com>
2016-12-18 21:51:21 -0500

    fixed timezone issue

--- js/student-calendar.js
+++ js/student-calendar.js
@@ -79,21 +79,11 @@ $(document).ready(function() {
                       May:"May", Jun:"June", Jul:"July", Aug:"August", 
                       Sep:"September", Oct:"October", Nov:"November", Dec:"December"};
 
-            var stime = String(new Date(calEvent.start.format()));/*.replace(
-                /\w{3} (\w{3}) (\d{2}) (\d{4}) (\d{2}):(\d{2}):[^(]+\(([A-Z]{3})\)/,
-                function($0,$1,$2,$3,$4,$5,$6){
-                    return MM[$1]+" "+$2+", "+$3+" - "+$4%12+":"+$5+(+$4>12?"PM":"AM")+" "+$6 
-                }
-            );*/
+            var stime = String(calEvent.start.format("MMMM DD, YYYY - hh:mmA"));
 
             var etime;
             if (calEvent.end != undefined) {
-                etime = String(new Date(calEvent.end.format())).replace(
-                    /\w{3} (\w{3}) (\d{2}) (\d{4}) (\d{2}):(\d{2}):[^(]+\(([A-Z]{3})\)/,
-                    function($0,$1,$2,$3,$4,$5,$6){
-                        return MM[$1]+" "+$2+", "+$3+" - "+$4%12+":"+$5+(+$4>12?"PM":"AM")+" "+$6 
-                    }
-                );
+                etime = String(calEvent.end.format("MMMM DD, YYYY - hh:mmA"));
             }
 
             var message = 'Event: ' + calEvent.title + 
